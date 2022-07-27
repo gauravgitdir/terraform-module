00393836a6924a79d4c580df81bd5c02e4a5815e
gauravgitdir <57554689+gauravgitdir@users.noreply.github.com>
2022-07-27 16:30:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,37 +1,30 @@
 
 variable "db_engine" {
-default = "aurora"
 type = string
 }
 
 variable "account_id" {
-default = "410533792414"
 type = number 
 }
 
 variable "db_cluster_instance_name" {
-default = "yashinsta"
 type = string
 }
 
 variable "db_admin_username" {
-default = "yash"
 type = string
 }
 
 variable "db_name" {
-default = "yashdb"
 type = string
 }
 
 
 variable "db_admin_password" {
-default = "yash123"
 type = string
 }
 
 variable "instance_count" {
-default = 2
 type = number
 }
 
